f34b850dc332aba5ed38d3529307a9d17e301df3
Balint Takacs <takbal@gmail.com>
2023-03-08 22:08:40 +0100

    whitespace

--- src/ptb/plots.py
+++ src/ptb/plots.py
@@ -3,6 +3,7 @@ import os
 from pathlib import Path
 from uuid import uuid4
 
+
 def disp(fig, title="figure", show=True, saveto=None, xsize=None, ysize=None, server_dir="/tmp/figures", include_plotlyjs="cdn"):
     """
     save or diplay a plotly plot through the figure server
